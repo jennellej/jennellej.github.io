9964b2ca8c4882509c204b37ed8265e1e248acca
ethanglide <ecook2489@gmail.com>
2024-05-21 20:35:46 -0400

    Improve mobile header

--- jennelle-app/src/components/Header.tsx
+++ jennelle-app/src/components/Header.tsx
@@ -4,16 +4,37 @@ import { useState } from "react"
 import { Link } from "react-router-dom";
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(true);
 
   return (
     <header className="p-10 flex flex-col gap-10">
+      <MenuButton onClick={() => setMenuOpen(!menuOpen)} />
+
       <Link
         to="/"
         className="text-4xl md:text-5xl text-center">
         Jennelle Jagdeo
       </Link>
-      <div className="flex flex-col sm:flex-row gap-2 sm:gap-12 self-center text-center">
+
+      {menuOpen && <NavMenu />}
+    </header>
+  )
+}
+
+function MenuButton({ onClick }: { onClick?: () => void }) {
+  return (
+    <button className="md:invisible m-2 h-10 w-10 rounded-md
+    hover:bg-blue-900 active:bg-blue-800
+      transition-colors duration-200 absolute left-3 top-12"
+      onClick={onClick}>
+      <FontAwesomeIcon icon={faBars} size="lg" />
+    </button>
+  )
+}
+
+function NavMenu({ onClick }: { onClick?: () => void }) {
+  return (
+    <nav className="flex flex-col sm:flex-row gap-2 sm:gap-12 self-center text-center">
         <Link 
           to="/blog"
           className="text-xl font-bold underline hover:text-slate-200 active:text-slate-300"
@@ -50,57 +71,6 @@ export default function Header() {
         >
           Contact
         </Link>
-      </div>
-    </header>
-  )
-}
-
-function MenuButton({ onClick }: { onClick?: () => void }) {
-  return (
-    <button className="m-2 h-10 w-10 rounded-md
-    hover:bg-blue-900 active:bg-blue-800
-      transition-colors duration-200"
-      onClick={onClick}>
-      <FontAwesomeIcon icon={faBars} size="lg" />
-    </button>
-  )
-}
-
-function NavMenu({ onClick }: { onClick?: () => void }) {
-  return (
-    <nav className="flex flex-col gap-y-2 pl-5">
-      <Link 
-        to="/" 
-        onClick={onClick}
-        className="text-lg text-jenny_gold 
-          hover:underline active:text-yellow-600"
-      >
-        Home
-      </Link>
-      <Link 
-        to="blog"
-        onClick={onClick}
-        className="text-lg text-jenny_gold 
-          hover:underline active:text-yellow-600"
-      >
-        Blog
-      </Link>
-      <Link 
-        to="gallery"
-        onClick={onClick}
-        className="text-lg text-jenny_gold 
-          hover:underline active:text-yellow-600"
-      >
-        Gallery
-      </Link>
-      <Link 
-        to="contact"
-        onClick={onClick}
-        className="text-lg text-jenny_gold 
-          hover:underline active:text-yellow-600"
-      >
-        Contact
-      </Link>
-    </nav>
+      </nav>
   )
 }
